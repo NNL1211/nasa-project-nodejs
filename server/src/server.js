9029ef75e5ff547ec5883621ff1f41f72ddd91e0
NNL1211 <nnhatlam1211@gmail.com>
2023-02-27 15:27:53 +0700

    set up mongodb

--- server/src/server.js
+++ server/src/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 require('dotenv').config();
 
 const app = require('./app');
-// const { mongoConnect } = require('./services/mongo');
+const { mongoConnect } = require('./services/mongo');
 const { loadPlanetsData } = require('./models/planets.model');
 const { loadLaunchData } = require('./models/launches.model');
 
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 8000;
 const server = http.createServer(app);
 
 async function startServer() {
-  // await mongoConnect();
+  await mongoConnect();
   await loadPlanetsData();
   await loadLaunchData();
   
